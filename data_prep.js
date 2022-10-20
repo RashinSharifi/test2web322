67ec789fe7197e8db4929bdc75dc345af9324656
RashinSharifi <113576230+RashinSharifi@users.noreply.github.com>
2022-10-20 22:38:28 +0330

    Update data_prep.js

--- data_prep.js
+++ data_prep.js
@@ -1,26 +1,17 @@
 var cpa = [];
-
+var students = [];
 
 const fs = require('fs');
-function initialize() {
-
-    return new Promise(function (resolve, reject) {
-
-        fs.readFile('./data/cpa.json', (err, data) => {
-            if (err) reject("Failure to read file cpa.json!");
-            cpa = JSON.parse(data);
-            console.log(cpa.length);
-        });
-        resolve();
-
-    });
-};
 
 function prep() {
     return new Promise(function (resolve, reject) {
-        if (cpa.length == 0)
-            reject("no result returned");
-        resolve(cpa);
+        fs.readFile('./students.json', (err, data) => {
+            if (err) reject("unable to read file");
+            students = JSON.parse(data);
+            console.log(students.length);
+        });
+
+        resolve(students);
     });
 
 };
@@ -28,6 +19,12 @@ function prep() {
 
 function cpa() {
     return new Promise(function (resolve, reject) {
+        fs.readFile('./data/cpa.json', (err, data) => {
+            if (err) reject("Failure to read file cpa.json!");
+            cpa = JSON.parse(data);
+            console.log(cpa.length);
+        });
+
         if (cpa.length == 0)
             reject("no result returned");
         resolve(cpa);
@@ -38,13 +35,13 @@ function cpa() {
 
 function highGPA() {
     return new Promise(function (resolve, reject) {
-        if (cpa.length == 0)
-            reject("no result returned");
+        if (students.length == 0)
+            reject("Failed finding the student with the highest GPA");
         
-        var max=cpa[0];
-        for(let i=0; i<cpa.length; i++){
-            if (cpa[i].gpa>max.gpa)
-                max=cpa[i];
+        var max=students[0];
+        for(let i=0; i<students.length; i++){
+            if (students[i].gpa>max.gpa)
+                max=students[i];
          
         }
         
@@ -52,4 +49,39 @@ function highGPA() {
     });
 
 };
-module.exports = {prep,cpa,initialize,highGPA};
+
+ function allStudents(){
+    return new Promise(function (resolve, reject) {
+        if (students.length == 0)
+            reject("no result returned");
+                      
+        resolve(students);
+    });
+ };
+
+ function addStudent(studentData) {
+    return new Promise(function (resolve, reject) {
+        
+        console.log(studentData);
+
+        students.push(studentData);
+        
+        resolve();
+    });
+};
+
+function getStudentByID(ID) {
+    return new Promise((resolve, reject) => {
+        console.log(ID);
+         for (let i=0; i<students.length; i++) {
+            //console.log(students[i].studId);
+             if (students[i].studId == ID) {
+                 resolve(students[i]);
+             }
+         }
+
+        reject("no result returned");
+    });
+}
+
+module.exports = {prep,cpa,highGPA,allStudents,addStudent,getStudentByID};
